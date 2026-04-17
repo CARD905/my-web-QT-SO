714953d83e954b7792119adddc88a7cbddf32d31
CARD905 <s6606021621011@email.kmutnb.ac.th>
2026-04-17 12:13:17 +0700

    four

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -1,16 +1,24 @@
+// import { Pool } from "pg";
+
+// export const pool = new Pool(
+//   process.env.DATABASE_URL
+//     ? {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: { rejectUnauthorized: false },
+//       }
+//     : {
+//         user: "SuperAdminBM",
+//         host: "localhost",
+//         database: "my_database",
+//         password: "12345678",
+//         port: 5432,
+//       }
+// );
 import { Pool } from "pg";
 
-export const pool = new Pool(
-  process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      }
-    : {
-        user: "SuperAdminBM",
-        host: "localhost",
-        database: "my_database",
-        password: "12345678",
-        port: 5432,
-      }
-);
+const isProduction = !!process.env.DATABASE_URL;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
